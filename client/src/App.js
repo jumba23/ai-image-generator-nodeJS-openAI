e3da406aef739ffa8815544311c6f06bfaf204dc
Goran SD <g.cvetic23@gamail.com>
2022-12-11 22:12:01 -0800

    tailwind configured

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,5 @@
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      Hello World
-        </header>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
