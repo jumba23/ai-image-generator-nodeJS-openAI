066c0d250e2ebbb46ac8a6f66e33896662a172c4
Goran SD <g.cvetic23@gamail.com>
2022-12-12 11:06:39 -0800

    styling removed and new themes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,31 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="container mx-auto">
+      <form flex flex-col mx-auto>
+        <div className="flex flex-col mx-auto">
+          <input
+            type="text"
+            name="prompt"
+            id="prompt"
+            className="block w-full py-2 pr-3 bg-white border rounded-md shadow-sm placeholder:italic placeholder:text-slate-400 border-slate-300 pl-9 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
+            placeholder="Type image description"
+          />
+          <div>
+            <select name="size" id="size">
+              <option value="small">Small</option>
+              <option value="medium" selected>
+                Medium
+              </option>
+              <option value="large">Large</option>
+            </select>
+          </div>
+          <button className="px-6 py-2 text-black rounded-full bg-veryPaleRed hover:bg-darkBlue hover:text-white ">
+            Generate
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
