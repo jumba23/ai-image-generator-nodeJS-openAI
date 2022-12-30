3f5b714b3f8328492a3ef96f9d19c005247d8547
Goran SD <g.cvetic23@gmail.com>
2022-12-30 00:38:48 -0800

    styling finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,34 @@
 function App() {
   return (
-    <div className="w-2/3 h-screen pt-5 mx-auto border-2">
-      <h1 className="p-3 text-3xl font-bold text-center">
+    <div className="flex flex-col w-2/3 h-screen pt-5 mx-auto bg-red-400 rounded-xl">
+      <h1 className="pb-3 text-3xl italic font-bold text-center">
         Try DALL·E 2, a new AI system!
       </h1>
-      <form className="flex flex-col items-center w-2/3 m-auto border-2">
-        {/* <div className="flex flex-col mx-auto"> */}
+      <form className="flex flex-col items-center w-3/4 px-8 py-5 m-auto border-2 border-orange-900 shadow-xl rounded-xl bg-sky-500">
         <input
           type="text"
           name="prompt"
           id="prompt"
-          className="block w-full py-2 pr-3 bg-white border rounded-md shadow-sm placeholder:italic placeholder:text-slate-400 border-slate-300 pl-9 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
+          className="block w-full py-2 pr-3 mb-8 bg-white border rounded-md shadow-sm placeholder:italic placeholder:text-slate-400 border-slate-300 pl-9 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
           placeholder="Type image description"
         />
-        <div className="fex ">
-          <select name="size" id="size">
-            <option value="small">Small</option>
-            <option value="medium" selected>
-              Medium
-            </option>
-            <option value="large">Large</option>
-          </select>
+        <div className="flex justify-between w-full">
+          <div className="flex flex-col gap-1">
+            <h3 className="py-3">Select image size</h3>
+            <select name="size" id="size" className="border rounded-md">
+              <option value="small">Small</option>
+              <option value="medium" selected>
+                Medium
+              </option>
+              <option value="large">Large</option>
+            </select>
+          </div>
+          <button className="px-6 py-2 mt-8 text-black rounded-full bg-veryPaleRed hover:bg-darkBlue hover:text-white ">
+            Generate
+          </button>
         </div>
-        <button className="px-6 py-2 text-black rounded-full bg-veryPaleRed hover:bg-darkBlue hover:text-white ">
-          Generate
-        </button>
-        {/* </div> */}
       </form>
+      <div className="flex items-center justify-center h-2/3"></div>
     </div>
   );
 }
